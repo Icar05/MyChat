87d73b26d9736128e30f9625a24446fb31ef6e90
Icar05 <galiatkin.o@gmail.com>
2015-06-09 15:29:17 +0300

    Добавил в новую активити список потениальних друзей
    1 Надо сделать вивод максимум 10 и скролл
    2 Обработчик нажатия Добавить
    3 Кнопку для возвращения домой

--- app/src/main/java/com/example/pinguin_linuxoid/mychat/Find_friends.java
+++ app/src/main/java/com/example/pinguin_linuxoid/mychat/Find_friends.java
@@ -7,25 +7,30 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 
-public class Find_friends extends Activity implements View.OnClickListener {
+public class Find_friends extends Activity {
 
-    TextView title_Add_friends;
+    Send_data sd;
     TextView title_result;
     ListView lv_Add_friends;
-    Button btn_Add_friends;
     ArrayList<Map<String, Object>> all_users;
-    String[] contact = {"Panda", "Chapaye", "JJ", "Icar"};
+    String[] contact = {""};
+    Button btn_Add;
     int[] to = {R.id.temp_message};
     final String ATTRIBUTE_NAME_TEXT = "text";
     String[] from = {ATTRIBUTE_NAME_TEXT};
-    String friend = null;
+    String data, friend, username = null;
+    ProgressBar pb;
+    final String PARSER = "`";
+
 
 
     @Override
@@ -33,45 +38,77 @@ public class Find_friends extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_friends);
 
-        title_Add_friends = (TextView)findViewById(R.id.title_Add_friends);
-        title_Add_friends.setTextColor(Color.GREEN);
-        title_result = (TextView)findViewById(R.id.title_Add_friends_result);
-        lv_Add_friends = (ListView)findViewById(R.id.lv_Add_friends);
-        btn_Add_friends = (Button)findViewById(R.id.btn_Add_friends);
-        btn_Add_friends.setOnClickListener(this);
+
+        title_result = (TextView) findViewById(R.id.title_Add_friends_result);
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+        lv_Add_friends = (ListView) findViewById(R.id.lv_Add_friends);
+        btn_Add = (Button)findViewById(R.id.btn_Add_friends);
+
+        username =getIntent().getStringExtra("name");
+        data = "names" + PARSER + username + PARSER + "@";
+        sd = new Send_data(data);
+        sd.execute();
 
 
-        all_users = new ArrayList<>(contact.length);
-        Map<String, Object> m_f;
+        try {
+            String temp = sd.get().toString();
 
 
-        for (int i = 0; i < contact.length; i++) {
-            m_f = new HashMap<String, Object>();
-            m_f.put(ATTRIBUTE_NAME_TEXT, contact[i]);
-            all_users.add(m_f);
+            if(temp.equals("Unknown host") || temp.equals("No connecting to internet! "))
+            {
+                title_result.setText(temp);
+                btn_Add.setEnabled(false);
+            }
+
+            else {
+
+                contact = temp.split(PARSER);
+
+                all_users = new ArrayList<>(contact.length);
+                Map<String, Object> m_f;
+
+                for (int i = 0; i < contact.length; i++) {
+                    m_f = new HashMap<String, Object>();
+                    m_f.put(ATTRIBUTE_NAME_TEXT, contact[i]);
+                    all_users.add(m_f);
+                }
+
+                SimpleAdapter sFriends = new SimpleAdapter(this, all_users, R.layout.temp, from, to);
+                lv_Add_friends.setAdapter(sFriends);
+                lv_Add_friends.setBackgroundColor(Color.RED);
+                lv_Add_friends.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        friend = contact[position];
+                        title_result.setText(friend);
+                        title_result.setTextColor(Color.WHITE);
+                    }
+                });
+
+            }
+
+        } catch (InterruptedException e) {
+          e.fillInStackTrace();
+        } catch (ExecutionException e) {
+           e.fillInStackTrace();
+        } finally {
+            sd.isCancelled();
+            pb.setVisibility(View.GONE);
         }
 
-        SimpleAdapter sFriends = new SimpleAdapter(this, all_users, R.layout.temp, from, to);
-        lv_Add_friends.setAdapter(sFriends);
-        lv_Add_friends.setBackgroundColor(Color.RED);
 
-        lv_Add_friends.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                friend = contact[position];
-                 title_result.setText(friend);
-            }
-        });
     }
 
 
-    @Override
-    public void onClick(View v) {
+    public void onClick_F(View v) {
         switch (v.getId())
         {
-            case R.id.btn_Add_friends :  title_result.setText("Click Click"); break;
+
+            case R.id.btn_Add_friends :           title_result.setText("Click Click");
+                                                  title_result.setTextColor(Color.RED);
+                                                  break;
             default: break;
         }
 
